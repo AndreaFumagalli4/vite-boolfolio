820e320916c8f9a9089726aa421802212e4566b4
AndreaFumagalli4 <98.fumagalli.andrea@gmail.com>
2023-03-02 17:17:16 +0100

    added project card component

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,12 +1,19 @@
 <script>
 import axios from 'axios';
+import ProjectCard from './ProjectCard.vue';
 
 export default{
     name: 'AppMain',
 
+    components:{
+        ProjectCard,
+
+    },
+
     data() {
         return {
             apiUrl: 'http://127.0.0.1:8000/api/projects',
+            projectsList: [],
         }
     },
 
@@ -16,8 +23,9 @@ export default{
                 params: {
                 }
             })
-            .then(function (response) {
-                console.log(response.data.results.data);
+            .then((response) => {
+                this.projectsList = response.data.results.data;
+                console.log(this.projectsList);
             })
             .catch(function (error) {
                 console.warn(error);
@@ -32,9 +40,22 @@ export default{
 </script>
 
 <template>
-    <h1>
-        Projects
-    </h1>
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h1>
+                        Projects
+                    </h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-4 g-3">
+                    <ProjectCard />
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
 
 <style lang="scss">
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    name: 'ProjectCard',
+
+}
+</script>
+
+<template>
+    <h2>
+        ProjectCard component
+    </h2>
+</template>
+
+<style lang="scss">
+    
+</style>
