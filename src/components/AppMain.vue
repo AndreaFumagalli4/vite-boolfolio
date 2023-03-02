bb9b177703a9205bc01b470e0ccfe8e76695ff8d
AndreaFumagalli4 <98.fumagalli.andrea@gmail.com>
2023-03-02 17:09:32 +0100

    expose api in console

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,9 +1,40 @@
 <script>
+import axios from 'axios';
 
+export default{
+    name: 'AppMain',
+
+    data() {
+        return {
+            apiUrl: 'http://127.0.0.1:8000/api/projects',
+        }
+    },
+
+    methods: {
+        getProjects(){
+            axios.get(this.apiUrl, {
+                params: {
+                }
+            })
+            .then(function (response) {
+                console.log(response.data.results.data);
+            })
+            .catch(function (error) {
+                console.warn(error);
+            });
+        }
+    },
+
+    created(){
+        this.getProjects();
+    }
+}
 </script>
 
 <template>
-  
+    <h1>
+        Projects
+    </h1>
 </template>
 
 <style lang="scss">
